ce15178f50fa701b0d911c59300870f28e1c4848
Oleksandr FIlippov <filya170895@gmail.com>
2023-10-22 15:12:52 +0200

    finished logic sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,113 @@
-import Container from 'components/Container/Container';
-import { HeaderWrapper, Logo, Nav, Wrapper } from './Header.styled';
+// import Container from 'components/Container/Container';
+import {
+  BurgerBtn,
+  BurgerIcon,
+  CloseIcon,
+  HeaderContainer,
+  HeaderWrapper,
+  LinkList,
+  Logo,
+  Nav,
+  NaviLink,
+  Overlay,
+  SideBarNav,
+  Wrapper,
+} from './Header.styled';
 import { NavLink } from 'react-router-dom';
 import logo from '../../images/icons/valorant-logo.svg';
+import { useEffect, useRef, useState } from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const sideBarRef = useRef(null);
+
+  useOnClickOutside(sideBarRef, () => setIsSideBarOpen(false));
+
+  const toggleSideBar = () => {
+    setIsSideBarOpen(!isSideBarOpen);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <HeaderWrapper>
-      <Container>
+    <HeaderWrapper $scrolled={scrolled.toString()}>
+      <HeaderContainer>
         <Wrapper>
           <NavLink to={'/'}>
             <Logo src={logo} alt="logo" />
           </NavLink>
+          <BurgerBtn type="button" onClick={toggleSideBar}>
+            {isSideBarOpen ? <CloseIcon /> : <BurgerIcon />}
+          </BurgerBtn>
+          <Overlay $isOpen={isSideBarOpen.toString()}>
+            <SideBarNav ref={sideBarRef}>
+              <ul>
+                <li>
+                  <NaviLink onClick={() => setIsSideBarOpen(false)} to={'/'}>
+                    Home
+                  </NaviLink>
+                </li>
+                <li>
+                  <NaviLink
+                    onClick={() => setIsSideBarOpen(false)}
+                    to={'/agents'}
+                  >
+                    Agents
+                  </NaviLink>
+                </li>
+                <li>
+                  <NaviLink
+                    onClick={() => setIsSideBarOpen(false)}
+                    to={'/maps'}
+                  >
+                    Maps
+                  </NaviLink>
+                </li>
+                <li>
+                  <NaviLink
+                    onClick={() => setIsSideBarOpen(false)}
+                    to={'/weapons'}
+                  >
+                    Weapons
+                  </NaviLink>
+                </li>
+              </ul>
+            </SideBarNav>
+          </Overlay>
           <Nav>
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'/agents'}>Agents</NavLink>
-            <NavLink to={'/maps'}>Maps</NavLink>
-            <NavLink to={'/weapons'}>Weapons</NavLink>
+            <LinkList>
+              <li>
+                <NaviLink to={'/'}>Home</NaviLink>
+              </li>
+              <li>
+                <NaviLink to={'/agents'}>Agents</NaviLink>
+              </li>
+              <li>
+                <NaviLink to={'/maps'}>Maps</NaviLink>
+              </li>
+              <li>
+                <NaviLink to={'/weapons'}>Weapons</NaviLink>
+              </li>
+            </LinkList>
           </Nav>
         </Wrapper>
-      </Container>
+      </HeaderContainer>
     </HeaderWrapper>
   );
 };
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,12 +1,41 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineClose } from 'react-icons/md';
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  padding-left: 15px;
+  padding-right: 15px;
+
+  margin-left: auto;
+  margin-right: auto;
+
+  @media screen and (min-width: 768px) {
+    max-width: 768px;
+    padding: 0 20px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    max-width: 1280px;
+    padding: 0 40px;
+  }
+`;
 
 export const HeaderWrapper = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-bottom: 1px solid black;
+
+  background-color: ${props =>
+    props.$scrolled !== 'false' ? 'rgba(90, 90, 90, 0.6)' : 'transparent'};
+  transition: background-color 0.3s ease-in-out;
+  z-index: 1000;
 `;
 
 export const Wrapper = styled.div`
@@ -18,12 +47,73 @@ export const Wrapper = styled.div`
 export const Logo = styled.img`
   width: 50px;
   height: 50px;
+`;
 
-  svg {
+export const BurgerBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1500;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const BurgerIcon = styled(GiHamburgerMenu)`
+  font-size: 25px;
+  color: red;
+`;
+
+export const CloseIcon = styled(MdOutlineClose)`
+  font-size: 25px;
+  color: red;
+`;
+
+export const Overlay = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(90, 90, 90, 0.6);
+  transition: all 250ms ease-in-out;
+  opacity: ${props => (props.$isOpen === 'false' ? '0' : '1')};
+  visibility: ${props => (props.$isOpen === 'false' ? 'hidden' : 'visible')};
+`;
+
+export const SideBarNav = styled.nav`
+  position: fixed;
+  top: 55px;
+  bottom: 15px;
+  left: 15px;
+  right: 15px;
+  background-color: lightgray;
+  border-radius: 10px;
+  padding: 15px;
+
+  @media screen and (min-width: 768px) {
+    display: none;
   }
 `;
 
 export const Nav = styled.nav`
+  display: none;
+
+  @media screen and (min-width: 768px) {
+    display: block;
+  }
+`;
+
+export const LinkList = styled.ul`
   display: flex;
   gap: 20px;
+`;
+
+export const NaviLink = styled(NavLink)`
+  display: block;
+  padding: 7px 0;
+  font-weight: 500;
 `;
